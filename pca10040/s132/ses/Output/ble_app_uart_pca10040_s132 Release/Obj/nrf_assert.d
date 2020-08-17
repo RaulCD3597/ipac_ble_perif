e2db7291b285d995323e053621dc32a4a7db2091
Raul Camacho <raulc3597@gmail.com>
2020-08-17 14:48:36 -0300

    ** Primer commit  **

---
+++ pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Release/Obj/nrf_assert.d
@@ -0,0 +1,24 @@
+Output/ble_app_uart_pca10040_s132\ Release/Obj/nrf_assert.o: \
+ /Users/raulcamacho/Documents/Nordic/nRF5_SDK_17.0.0_9d13099/components/libraries/util/nrf_assert.c \
+ /Users/raulcamacho/Documents/Nordic/nRF5_SDK_17.0.0_9d13099/components/libraries/util/nrf_assert.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdint.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ /Users/raulcamacho/Documents/Nordic/nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_error.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdio.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/__crossworks.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdbool.h \
+ /Users/raulcamacho/Documents/Nordic/nRF5_SDK_17.0.0_9d13099/components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ /Users/raulcamacho/Documents/Nordic/nRF5_SDK_17.0.0_9d13099/components/libraries/util/nordic_common.h \
+ /Users/raulcamacho/Documents/Nordic/nRF5_SDK_17.0.0_9d13099/components/libraries/util/app_error_weak.h
